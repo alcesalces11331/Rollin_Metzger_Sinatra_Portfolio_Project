ae5f3f4fdc04846ad01b13a825637b7f7c5e5730
alcesalces11331 <rollin.metzger@gmail.com>
2018-05-16 13:18:29 -0500

    application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,26 @@
-filler
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+	configure do
+		set :public_folder, 'public'
+		set :views, 'app/views'
+		enable :sessions
+		set :session_secret, "rollad20"
+	end
+
+	get '/' do
+		erb :index
+	end
+
+	helpers do
+
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
+end
