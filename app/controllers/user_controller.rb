b26788ca80b4477d263a6e895555840c782f680d
alcesalces11331 <rollin.metzger@gmail.com>
2018-05-17 11:43:24 -0500

    added GET/POST /signup actions to user_controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,20 @@
 class UserController < ApplicationController
-	
+
+	get '/signup' do
+		!logged_in? ? (erb :'/users/create_user') : (redirect '/characters')
+	end
+
+	post '/signup' do
+		if !logged_in?
+			if params[:username] == "" || params[:password] == "" || params[:email] == ""
+				redirect '/signup'
+			else
+				@user = User.create(username: params[:username], password: params[:password], email: params[:email])
+				session[:user_id] = @user_id
+				redirect '/characters'
+			end
+		else
+			redirect '/signup'
+		end
+	end
 end
