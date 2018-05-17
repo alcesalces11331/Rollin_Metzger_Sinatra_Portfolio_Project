c93078b26a15571fb8cdc685120a7570364ba676
alcesalces11331 <rollin.metzger@gmail.com>
2018-05-17 11:56:05 -0500

    added GET /characters to character_controller

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,2 +1,12 @@
 class CharacterController < ApplicationController
+
+	get '/characters' do
+		if logged_in?
+			@user = current_user
+			@characters = Character.all
+			erb :'/characters/characters'
+		else
+			redirect '/login'
+		end
+	end
 end
