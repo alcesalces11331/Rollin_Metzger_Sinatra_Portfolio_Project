e9ed50b2f8d425c13a5b1aab2e981b63b5d43376
alcesalces11331 <rollin.metzger@gmail.com>
2018-05-20 12:59:17 -0500

    added racial bonuses columns to race table

--- db/migrate/03_create_races.rb
+++ db/migrate/03_create_races.rb
@@ -3,6 +3,8 @@ class CreateRaces < ActiveRecord::Migration
 		create_table :races do |t|
 			t.string :name
 			t.string :language
+			t.string :bonus_1
+			t.string :bonus_2
 			t.integer :abs_1
 			t.integer :abs_2
 		end
