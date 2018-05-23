49a6263d8231de1114e06f432bca8ec278dd735b
Rollin Metzger <rollin.metzger@gmail.com>
2018-05-23 10:46:48 -0500

    added GET action races/new; added POST action /races

--- app/controllers/race_controller.rb
+++ app/controllers/race_controller.rb
@@ -9,4 +9,26 @@ class RaceController < ApplicationController
 		@race = Race.find_by_slug(params[:slug])
 		erb :'/races/show'
 	end
+
+	get '/races/new' do
+		if logged_in?
+			erb :'/races/new'
+		else
+			redirect '/login'
+		end
+	end
+
+	post '/races' do
+		if logged_in?
+			if params[:races] = ""
+				redirect '/races/new'
+			else
+				@race = current_user.races.create(params[:race])
+				@race.save
+				redirect "/characters/#{@race.slug}"
+			end
+		else
+			redirect '/login'
+		end
+	end
 end
