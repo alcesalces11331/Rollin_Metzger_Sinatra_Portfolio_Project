1fd7801031b99ba6dc7c3b15004b1de093356f58
Rollin Metzger <rollin.metzger@gmail.com>
2018-05-24 12:26:57 -0500

    added more columns to klass table;initial start on klasses.erb

--- db/migrate/04_create_klasses.rb
+++ db/migrate/04_create_klasses.rb
@@ -2,6 +2,9 @@ class CreateKlasses < ActiveRecord::Migration[4.2]
 	def change
 		create_table :klasses do |t|
 			t.string :name
+			t.string :hitdie
+			t.string :proficencies
+			t.string :ability
 		end
 	end
 end
