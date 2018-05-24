881b625e0a293ff609699796014de507c6538205
Rollin Metzger <rollin.metzger@gmail.com>
2018-05-24 12:47:10 -0500

    POST action /klasses

--- app/controllers/klass_controller.rb
+++ app/controllers/klass_controller.rb
@@ -16,4 +16,18 @@ class KlassController < ApplicationController
 			redirect '/login'
 		end
 	end
+
+	post '/klasses' do
+		if logged_in?
+			if params[:klass] == ""
+				redirect '/klasses/new'
+			else
+				@klass = current_user.klasses.create(params[:klass])
+				flash[:message] = "Class Created"
+				redirect "/klasses/#{@klass.slug}"
+			end
+		else
+			redirect '/login'
+		end
+	end
 end
