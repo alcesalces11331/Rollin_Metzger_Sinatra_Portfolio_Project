946e947ae9c54611ea1e6cad58a7d5030269298f
Rollin Metzger <rollin.metzger@gmail.com>
2018-05-26 08:59:02 -0500

    updated race_controller

--- app/controllers/race_controller.rb
+++ app/controllers/race_controller.rb
@@ -1,65 +1,47 @@
 class RaceController < ApplicationController
 
 	get '/races' do
-		if logged_in?
-			@races = Race.all
-			erb :'/races/index'
-		else
-			redirect '/login'
-		end
+		login_validate
+		@races = Race.all
+		erb :'/races/index'
 	end
 
 	get '/races/new' do
-		if logged_in?
-			@race 
-			erb :'/races/new'
-		else
-			redirect '/login'
-		end
+		login_validate
+		@race 
+		erb :'/races/new'
 	end
 
 	post '/races' do
-		if logged_in?
-			if params[:race] == ""
-				redirect '/races/new'
-			else
-				@race = current_user.races.create(params[:race])
-				flash[:message] = "Race Created"
-				redirect "/races/#{@race.slug}"
-			end
+		login_validate
+		if params[:race] == ""
+			redirect '/races/new'
 		else
-			redirect '/login'
+			@race = current_user.races.create(params[:race])
+			flash[:message] = "Race Created"
+			redirect "/races/#{@race.slug}"
 		end
 	end
 
 	get '/races/:slug' do
-		if logged_in?
-			@race = Race.find_by_slug(params[:slug])
-			erb :'/races/show'
-		else
-			redirect '/login'
-		end
+		login_validate
+		@race = Race.find_by_slug(params[:slug])
+		erb :'/races/show'
 	end
 
 	get '/races/:slug/edit' do 
-		if logged_in?
-			@race = Race.find_by_slug(params[:slug])
-			erb :'/races/edit'
-		else
-			redirect '/login'
-		end
+		login_validate
+		@race = Race.find_by_slug(params[:slug])
+		erb :'/races/edit'
 	end
 
 	patch '/races/:slug' do
-		if logged_in?
-			@race = Races.find_by_slug(params[:slug])
-			@race.update(params[:race])
-			@race.save
-			flash[:message] = "Race Updated"
-			redirect "/races/#{@race.slug}"
-		else
-			redirect '/login'
-		end
+		login_validate
+		@race = Races.find_by_slug(params[:slug])
+		@race.update(params[:race])
+		@race.save
+		flash[:message] = "Race Updated"
+		redirect "/races/#{@race.slug}"
 	end
 
 	delete '/races/:slug/delete' do
