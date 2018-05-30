7dad3ac96af5f4e33c450d800a0695d548841252
Rollin Metzger <rollin.metzger@gmail.com>
2018-05-29 21:22:32 -0500

    modified races table; generic races added to seeds

--- db/migrate/03_create_races.rb
+++ db/migrate/03_create_races.rb
@@ -5,8 +5,8 @@ class CreateRaces < ActiveRecord::Migration[4.2]
 			t.string :language
 			t.string :bonus_1
 			t.string :bonus_2
-			t.integer :abs_1
-			t.integer :abs_2
+			t.string :abs_1
+			t.string :abs_2
 			t.integer :user_id
 		end
 	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 races_list = [
-
+	{name: 'Human', language: 'Common', bonus_1: 'One Additional Feat', bonus_2: 'Skilled', abs_1: '+1 to any Ability', abs_2: '+1 to any Ability'},
+	{name: 'Dwarf', language: 'Dwarvish', bonus_1: 'Stonetouch', bonus_2: 'Stout', abs_1: '+2 to Constitution', abs_2: '-1 Dexterity'},
+	{name: 'Elf', language: 'Elvish', bonus_1: 'Fleetfoot', bonus_2: 'Feymind', abs_1: '+1 to Dexterity and Intelligence', abs_2: '-2 Constitution'},
+	{name: 'Gnome', language: 'Gnomish', bonus_1: 'Crazy', bonus_2: 'Bonkers', abs_1: '+1 to Everything', abs_2: '-2 to two Random Abilities'}
 ]
 
 races_list.each {|spec| new_race = Race.create(spec)}
