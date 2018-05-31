729a7559e3150636c738c853a61395b20145c63c
Rollin Metzger <rollin.metzger@gmail.com>
2018-05-30 19:28:03 -0500

    validates uniqueness of username, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 	
 	has_secure_password
 	validates :username, :email, :password, presence: true
+	validates :username, :email, uniqueness: true
 	validates :password, length: { in: 6..20,
 		message: 'must be between 6-20'}
 	
