9308fc4b09de321c5e238c877768c095ca898371
Rollin Metzger <rollin.metzger@gmail.com>
2018-05-30 19:14:54 -0500

    validates_associated for Character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,6 +2,10 @@ class Character < ActiveRecord::Base
 	include Slugifiable::InstanceMethods
 	extend Slugifiable::ClassMethods
 	
-	validates :name, :race, :klass, presence: true 
+	validates :name, :race, :klass, presence: true
+
+	validates_associated :klass
+	validates_associated :race
+
 	belongs_to :user
 end
